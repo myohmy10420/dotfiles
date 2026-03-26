8b5b64b09b2fd78e442c7a56b3a72e03d8367e5a
KurtHsu <kurt@psp-power.com.tw>
2026-03-26 10:36:04 +0800

    update config

---
+++ dot_config/private_secrets.zsh
@@ -0,0 +1,8 @@
+# GitLab MCP Server Token
+export GITLAB_PERSONAL_ACCESS_TOKEN=""
+
+# ClickUp MCP Server Token
+# Get token from: https://app.clickup.com/settings/apps
+export CLICKUP_API_TOKEN=""
+
+export OPENAI_API_KEY=""
